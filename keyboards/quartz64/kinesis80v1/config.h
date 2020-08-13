bcd06bf2be6dc85a16d8cc18fa318d7e7e21451a
DmNosachev <quartz64@gmail.com>
2020-08-13 14:17:11 +0300

    kinesis: layout tuning

---
+++ keyboards/quartz64/kinesis80v1/config.h
@@ -0,0 +1,59 @@
+/*
+Copyright 2019 DmNosachev
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x0000
+#define MANUFACTURER QMK
+#define PRODUCT Kinesis 80
+#define DESCRIPTION handwired Kinesis
+
+/* key matrix size */
+#define MATRIX_COLS 12 
+#define MATRIX_ROWS 7
+
+#define DIODE_DIRECTION COL2ROW
+
+#define DEBOUNCE 5
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+/* disable these deprecated features by default */
+#ifndef LINK_TIME_OPTIMIZATION_ENABLE
+  #define NO_ACTION_MACRO
+  #define NO_ACTION_FUNCTION
+#endif
+
+#define UNICODE_SELECTED_MODES UC_WINC
