eeb06f8f29ce4a15784fa8dca4be1b6b34a8fc09
DmNosachev <quartz64@gmail.com>
2020-12-18 12:58:50 +0300

    pinout, build options

--- keyboards/handwired/cumulus/config.h
+++ keyboards/handwired/cumulus/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define MATRIX_ROW_PINS { D2, D3, C6, C7, D5, D4, E6, D1 }
-#define MATRIX_COL_PINS { F0, F1, F4, F5, F6, F7, B6, B5, B4, D7, B0, B1, B2, B3}
+#define MATRIX_COL_PINS { D0, B7, B3, B2, B1, B0, D7, B4, B5, B6, F7, F6, F5, F4}
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
