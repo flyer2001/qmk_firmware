8084cfd261c019bd7f355c7db5356a346da0d19a
DmNosachev <quartz64@gmail.com>
2021-02-12 12:33:06 +0300

    midi layout

--- keyboards/g80_3700/keymaps/default/keymap.c
+++ keyboards/g80_3700/keymaps/default/keymap.c
@@ -74,7 +74,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MS_L,   KC_MS_D,  KC_MS_R,  KC_NO,
         KC_MPRV,   KC_MPRV,  KC_MNXT,  KC_WH_D,
         KC_MPLY,             KC_MYCM,  KC_NO
-        
     )
 };
 
