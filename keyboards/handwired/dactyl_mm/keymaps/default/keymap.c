215c1aaa8c52c8a1698ab56e3fcae65e54a020a2
DmNosachev <quartz64@gmail.com>
2021-05-17 13:21:28 +0300

    default keymap: thumb cluster remap

--- keyboards/handwired/dactyl_mm/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_mm/keymaps/default/keymap.c
@@ -27,19 +27,19 @@ empty template
 * |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
 * | Shift  |   Z  |   X  |   C  |   V  |   B  |                           |   N  |   M  |  ,.  |  .>  |  /?  | Shift  |
 * `--------+------+------+------+------+-------------,             ,------|------+------+------+------+------+--------'
-*                 |   [  |   ]  |  Alt |LOWER | GUI  |             | Ctrl |RAISE | Alt  |  -   |Insert|
+*                 |Insert|  Alt |LOWER |Space |Bkspc |             |  Del | Enter| RAISE|   [  |   ]  |
 *                 `----------------------------------|             |----------------------------------'
-                                       | Space| Bkspc|             |  Del | Enter|
+                                       |  -   | GUI  |             | Ctrl |  Alt |
                                        `-------------'             '-------------'
 */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_6x6(
-        KC_ESC,         KC_1, KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,  KC_7,    KC_8,    KC_9,   KC_0,    KC_EQL,
-        KC_TAB,         KC_Q, KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,  KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
-        LCTL_T(KC_GRV), KC_A, KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,  KC_J,    KC_K,    KC_L,   KC_SCLN, LT(_LOWER, KC_QUOT),
-        KC_LSFT,        KC_Z, KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,  KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                              KC_LBRC, KC_RBRC, KC_LALT, LOWER,  KC_LGUI,    KC_RCTRL, RAISE, KC_RALT, KC_MINS, KC_INS,
-                                                         KC_SPC, KC_BSPC,      KC_DEL, KC_ENTER
+        KC_ESC,         KC_1, KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,   KC_7,    KC_8,    KC_9,   KC_0,    KC_EQL,
+        KC_TAB,         KC_Q, KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
+        LCTL_T(KC_GRV), KC_A, KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, LT(_LOWER, KC_QUOT),
+        KC_LSFT,        KC_Z, KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                              KC_INS,  KC_LALT, LOWER,   KC_SPC,  KC_BSPC,   KC_DEL, KC_ENTER, RAISE,   KC_LBRC, KC_RBRC,
+                                                         KC_MINS, KC_LGUI,   KC_RCTRL, KC_RALT
     ),
 /*
 * ,-------------------------------------------,                           ,-------------------------------------------.
