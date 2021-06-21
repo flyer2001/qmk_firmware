5bb6d0ae290d24a91c46950a56def37092c2ee8d
DmNosachev <quartz64@gmail.com>
2021-06-21 10:50:40 +0000

    encoder api

--- keyboards/handwired/hp_46010a/keymaps/debug/keymap.c
+++ keyboards/handwired/hp_46010a/keymaps/debug/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) {
     if (clockwise) {
       tap_code(KC_PGUP);
@@ -38,6 +38,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
       tap_code(KC_PGDN);
     }
   }
+  return true;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
