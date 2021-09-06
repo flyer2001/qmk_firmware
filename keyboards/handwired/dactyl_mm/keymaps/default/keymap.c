2551f5be02ca2c584d287bafb537b6f45df1a6f9
DmNosachev <quartz64@gmail.com>
2021-09-06 14:56:54 +0000

    VIA support, proper info.json

--- keyboards/handwired/dactyl_mm/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_mm/keymaps/default/keymap.c
@@ -33,7 +33,7 @@ empty template
                                        `-------------'             '-------------'
 */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_6x6(
+    [_QWERTY] = LAYOUT(
         KC_ESC,         KC_1, KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,   KC_7,    KC_8,    KC_9,   KC_0,    KC_EQL,
         KC_TAB,         KC_Q, KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
         LCTL_T(KC_GRV), KC_A, KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, LT(_LOWER, KC_QUOT),
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        |      |      |             |      |      |
                                        `-------------'             '-------------'
 */
-    [_LOWER] = LAYOUT_6x6(
+    [_LOWER] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
         _______, _______, _______, _______, _______, _______,                       KC_HOME, _______, KC_UP,   _______,  KC_PGUP, KC_F12,
         _______, _______, _______, _______, _______, _______,                       KC_END,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, _______,
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        |      |      |             |      |      |
                                        `-------------'             '-------------'
 */
-    [_RAISE] = LAYOUT_6x6(
+    [_RAISE] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                       RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______,
         _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______,                       _______, _______, _______, _______, _______, _______,
         _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,                       _______, _______, _______, _______, _______, _______,
