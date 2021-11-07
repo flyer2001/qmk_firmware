342e9ba0111ec3ee481d79ec0fbcf0ccd2033dcb
Sergey Popyvanov <s.popyvanov@redmadrobot.com>
2021-11-07 21:25:12 +0400

    ver 1.0.1 add shift [

--- keyboards/kinesis80/keymaps/quartz64/keymap.c
+++ keyboards/kinesis80/keymaps/quartz64/keymap.c
@@ -52,7 +52,7 @@ enum custom_keycodes {
 * |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
 * |  CAPS  |   A  |   S  |   D  |   F  |   G  |                           |   H  |   J  |   K  |   L  |  ;:  | '"/NM  |
 * |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
-* |   [    |  Z   |   X  |   C  |   V  |   B  |                           |   N  |   M  |  ,.  |  .>  |  /?  |    ]   |
+* |[/Shift |  Z   |   X  |   C  |   V  |   B  |                           |   N  |   M  |  ,.  |  .>  |  /?  |    ]   |
 * `--------+------+------+------+------+-------                           `------+------+------+------+------+--------'
 *          |  `~  |  MC  |   [  |   ]  |                                         | Left | Down |  Up  | Right|
 *          `---------------------------'                                         `---------------------------'
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LALT_T(KC_EQL),   KC_1,   KC_2,   KC_3,   KC_4,    KC_5,      KC_6,   KC_7,   KC_8,    KC_9,   KC_0,      KC_MINS,
     KC_TAB,           KC_Q,   KC_W,   KC_E,   KC_R,    KC_T,      KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,      KC_LBRC,
     KC_CAPS,          HOME_A, HOME_S, HOME_D, HOME_F,  KC_G,      KC_H,   HOME_J, HOME_K,  HOME_L, HOME_SCLN, LT(_NM, KC_QUOT),
-    KC_LBRC,          KC_Z, KC_X,   KC_C,   KC_V,      KC_B,      KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,   KC_RBRC,
+    LSFT_T(KC_LBRC),          KC_Z, KC_X,   KC_C,   KC_V,      KC_B,      KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,   KC_RBRC,
                 KC_GRV, MO(_MC), KC_LBRC, KC_RBRC,                        KC_LEFT,KC_DOWN, KC_UP,  KC_RGHT,
 			                               KC_DEL,  KC_ENTER,                  KC_SPC, KC_BSPC,
                                               KC_HOME,                   KC_PGUP,
